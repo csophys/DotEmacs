ea5353c89830d8be240d16577cf5ab73cc8f8613
csophys <zjsy_cs@163.com>
2012-07-13 09:18:29 +0800

    company

---
+++ .emacs.d/auto-insert/insert.el
@@ -0,0 +1,5 @@
+;;;auto-insert 开始
+(setq inhibit-startup-message t);
+(insert "请按CTRL+V向下翻阅,ALT+V向上翻阅\n");
+;;;auto-insert 结束
+
--- .emacs.d/csophys/csophys_writingStyle.el
+++ .emacs.d/csophys/csophys_writingStyle.el
@@ -11,7 +11,8 @@
 (fset 'yes-or-no-p 'y-or-n-p);简化yes or no 选择
 (column-number-mode t);显示列号
 (setq default-fill-column 80);默认列数为80列
-
+(setq user-full-name "csophys");
+(setq user-mail-address "zjsy_cs@163.com")
 ;日期和时间显示
 (display-time-mode 1);打开时间x显示模式
 (setq display-time-24hr-format t) ;显示24小时制时间
@@ -226,11 +227,16 @@
 (setq auto-insert-alist
       '(
         ("\\.java$" . ["insert.java"  auto-update-java-source-file])
+		("\\.el$" . ["insert.el" default-autoinsert])
         ))
+
+(defun default-autoinsert ()
+  "default function when open the insert templete"
+  (goto-char (point-max))
+	)
 
 (defun auto-update-java-source-file ()
   "do something when open an java source"
-  (interactive)
   (goto-char (- (point-max) 1))
   (yas/expand)
 	)
