a209868a2663ce7b0d9fd53dadadf48fd6524485
csophys <zjsy_cs@163.com>
2012-07-24 09:43:15 +0800

    ajc-java

--- .emacs.d/.csophys_custom.el
+++ .emacs.d/.csophys_custom.el
@@ -5,6 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes nil)
  '(ecb-options-version "2.40")
+ '(ecb-source-path (quote (("//10.167.157.159/TestSet" "//10.167.157.159/TestSet") ("c:" "c:") ("//10.167.157.159/TestSet" "//10.167.157.159/TestSet") ("//10.167.157.159/TestSet" "//10.167.157.159/TestSet") ("//10.167.157.159/TestSet" "//10.167.157.159/TestSet") ("//10.167.157.159/TestSet" "//10.167.157.159/TestSet") ("//10.167.157.159/TestSet" "//10.167.157.159/TestSet") ("//10.167.157.159/TestSet" "//10.167.157.159/TestSet"))))
+ '(nxml-child-indent 4)
  '(org-agenda-files (quote ("~/schedule.org.gpg")))
  '(semantic-c-dependency-system-include-path (quote ("c:/Dev-Cpp/include" "D:/MinGW/include" "/usr/include")))
  '(semantic-java-dependency-system-include-path (quote ("C:\\Program Files\\Java\\jdk1.6.0_16\\src" "C:\\Program Files\\Java\\jdk1.6.0_20\\src"))))
